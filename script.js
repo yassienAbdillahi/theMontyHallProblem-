9e3af0f90a4b486e109bfcb531d7e01ed08f5037
y <johndoe@example.com>
2022-06-12 13:49:02 +0100

    Edited my whichDoorDoesHostFirstOpen function so that it writes into the html
    Closes #7

--- script.js
+++ script.js
@@ -64,53 +64,88 @@ door1Button.addEventListener("click", playerChooseDoor1);
 door2Button.addEventListener("click", playerChooseDoor2);
 door3Button.addEventListener("click", playerChooseDoor3);
 
+let revealedDoorinHTML = document.getElementById("initialReveal");
 
 function whichDoorDoesHostFirstOpen () {
     let revealedDoor;
     let b = Math.floor(Math.random() * 2 ) + 1; //this randomly assigns 1 or 2 to the variable b
     console.log(b);
    
-    //the following 12 lines determine what happens if the prize is behind door1
+    //the following 24 lines determine what happens if the prize is behind door1
     if (
       (doors.door1 == "ferrari") && (doorPlayerGuessed == "door1") && (b == 1)
-    ) {revealedDoor = "door2";}
+    ) {
+        revealedDoor = "door2";
+        revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
+    }
     else if (
       (doors.door1 == "ferrari") && (doorPlayerGuessed == "door1") && (b == 2)
-    ) {revealedDoor = "door3";}
+    ) {
+        revealedDoor = "door3";
+        revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
+    }
       else if (
         (doors.door1 == "ferrari") && (doorPlayerGuessed == "door2")
-      ){revealedDoor = "door3";}
+      ){
+          revealedDoor = "door3";
+          revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
+    }
     else if (
       (doors.door1 == "ferrari") && (doorPlayerGuessed == "door3")  
-    ) {revealedDoor = "door2";}    
+    ) {
+        revealedDoor = "door2";
+        revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
+    }    
   
-    //the following 12 lines determine what happens if the prize is behind door2
+    //the following 24 lines determine what happens if the prize is behind door2
     else if (
       (doors.door2 == "ferrari") && (doorPlayerGuessed == "door2") && (b == 1)
-    ) {revealedDoor = "door1";}
+    ) {
+        revealedDoor = "door1";
+        revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
+    }
     else if (
       (doors.door2 == "ferrari") && (doorPlayerGuessed == "door2") && (b == 2)
-    ) {revealedDoor = "door3";}
+    ) {
+        revealedDoor = "door3";
+        revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
+    }
       else if (
         (doors.door2 == "ferrari") && (doorPlayerGuessed == "door1")
-      ){revealedDoor = "door3";}
+      ){
+          revealedDoor = "door3";
+          revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
+        }
     else if (
       (doors.door2 == "ferrari") &&  (doorPlayerGuessed == "door3")
-    ) {revealedDoor = "door1";}
+    ) {
+        revealedDoor = "door1";
+        revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
+    }
   
-    //the following 12 lines determine what happens if the prize is behind door3 
+    //the following 24 lines determine what happens if the prize is behind door3 
     else if (
       (doors.door3 == "ferrari") &&  (doorPlayerGuessed == "door3") && (b == 1)
-    ) {revealedDoor = "door1";}
+    ) {
+        revealedDoor = "door1";
+        revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
+    }
     else if (
       (doors.door3 == "ferrari") &&  (doorPlayerGuessed == "door3") && (b == 2)
     ) {
       revealedDoor = "door2";
+      revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
     }
     else if (
       (doors.door3 == "ferrari") && (doorPlayerGuessed == "door1")
-    ) {revealedDoor = "door2";}
-    else {revealedDoor = "door1";}
+    ) {
+        revealedDoor = "door2";
+        revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
+}
+    else {
+        revealedDoor = "door1";
+        revealedDoorinHTML.innerHTML = `You chose ${doorPlayerGuessed}. The host opens ${revealedDoor}. There is a goat behind ${revealedDoor}`;
+    }
   
     console.log(revealedDoor);
   }
