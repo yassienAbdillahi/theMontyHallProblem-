eb192af63042d9ee9fb6f738d953ff805d31491d
y <johndoe@example.com>
2022-06-12 12:50:17 +0100

    Wrote functions that allow the player to choose a door by clicking boxes on the page
    Closes #2

--- script.js
+++ script.js
@@ -24,3 +24,42 @@ const doors = {
       doors.door3 = "ferrari";
     }
   }
+
+const possibleDoorsForPlayerToGuess = ["door1", "door2", "door3"];
+let doorPlayerGuessed;
+
+function playerChooseDoor1(){
+  whichDoorHasTheFerrari(); //the player choosing a door effectively starts the game
+  console.log(doors);
+ 
+  doorPlayerGuessed = possibleDoorsForPlayerToGuess[0];
+  console.log(doorPlayerGuessed);
+  whichDoorDoesHostFirstOpen (); //the player choosing a door determines which door the host will open
+}
+
+function playerChooseDoor2(){
+  whichDoorHasTheFerrari(); //the player choosing a door effectively starts the game
+  console.log(doors);
+
+  doorPlayerGuessed = possibleDoorsForPlayerToGuess[1];
+  console.log(doorPlayerGuessed);
+  whichDoorDoesHostFirstOpen (); //the player choosing a door determines which door the host will open
+}
+
+function playerChooseDoor3(){
+  whichDoorHasTheFerrari(); //the player choosing a door effectively starts the game
+  console.log(doors);
+ 
+  doorPlayerGuessed = possibleDoorsForPlayerToGuess[2];
+  console.log(doorPlayerGuessed);
+  whichDoorDoesHostFirstOpen (); //the player choosing a door determines which door the host will open
+}
+
+
+const door1Button = document.getElementById("door1Button");
+const door2Button = document.getElementById("door2Button");
+const door3Button = document.getElementById("door3Button");
+
+door1Button.addEventListener("click", playerChooseDoor1);
+door2Button.addEventListener("click", playerChooseDoor2);
+door3Button.addEventListener("click", playerChooseDoor3);
